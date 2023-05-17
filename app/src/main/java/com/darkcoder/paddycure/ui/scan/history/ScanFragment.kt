fb2b0b8aa1b8bc4debe95e2c1daf00822cb884f7
muhammad rifqi syatria <syatriarifqi@gmail.com>
2023-05-18 06:53:12 +0700

    setup folder

---
+++ app/src/main/java/com/darkcoder/paddycure/ui/scan/history/ScanFragment.kt
@@ -0,0 +1,27 @@
+package com.darkcoder.paddycure.ui.scan.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.darkcoder.paddycure.R
+
+
+class ScanFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scan, container, false)
+    }
+
+
+}
