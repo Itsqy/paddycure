d1e214a324c444e54a1544d63f3c234a6cbc834b
muhammad rifqi syatria <syatriarifqi@gmail.com>
2023-05-17 14:39:51 +0700

    slicing home

---
+++ app/src/main/java/com/darkcoder/paddycure/dummies/Hero.kt
@@ -0,0 +1,8 @@
+package com.darkcoder.paddycure.dummies
+
+data class Hero(
+
+    val id : String,
+    val name : String,
+    val img: String
+)
---
+++ app/src/main/java/com/darkcoder/paddycure/ui/auth/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.darkcoder.paddycure.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.darkcoder.paddycure.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
